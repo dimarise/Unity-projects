70ebc5b645007c11a94779731097755970b93164
dimarise <72493262+dimarise@users.noreply.github.com>
2020-10-15 13:19:58 +0300

    разбил 1 функцию на 2 чтобы небыло повторений кода

--- calculator/Assets/Scripts/calc.cs
+++ calculator/Assets/Scripts/calc.cs
@@ -1,12 +1,15 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class calc : MonoBehaviour
 {
     public Text CalculatorWindow;
-    private float _number1;
-    private float _number2;
+    private float _number1 =0.0f;
+    private float _number2 = 0.0f;
     private float _currentNumber;
     private float _result;
     private string operation;
@@ -20,45 +23,60 @@ public class calc : MonoBehaviour
 
     public void NumberPressed(int number)
     {
-        if (dotIsPressed)
-        {
-            _currentNumber = GetFloatedNumber(number, _currentNumber);
-            CalculatorWindow.text = _currentNumber.ToString();
-        }
-        else
+        if (!dotIsPressed)
         {
             CheckOperation(number);
         }
-/*        Debug.Log("operationIsPressed = " + operationIsPressed + "dotispressed " + dotIsPressed);
-        Debug.Log("input 1 = " + _number1 + "input 2 = " + _number2 + "current number" + _currentNumber);*/
+        else
+        {
+            _currentNumber = GetFloatedNumber(number, _currentNumber);
+            CalculatorWindow.text += _currentNumber.ToString();
+        }
     }
-
+    // работает лучше чем 2 функции ниже for some reason 
+    /*    private void CheckOperation(int NumberOnButton)
+        {
+            if (!operationIsPressed)
+            {
+                _number1 = _number1 != 0 ? (_number1 * 10) + NumberOnButton : NumberOnButton;
+                _currentNumber = _number1;
+                CalculatorWindow.text = "" + _currentNumber;
+            }
+            else
+            {
+                _number2 = _number2 != 0 ? (_number2 * 10) + NumberOnButton : NumberOnButton;
+                _currentNumber = _number2;
+                CalculatorWindow.text += ""+ _currentNumber;
+            }
+        }*/
     private void CheckOperation(int NumberOnButton)
     {
+        Debug.Log("number = " + NumberOnButton + " _number1 = " + _number1 + " number 2 = " + _number2);
         if (!operationIsPressed)
         {
-            _number1 = _number1 != 0 ? (_number1 * 10) + NumberOnButton : NumberOnButton;
-            _currentNumber = _number1;
-            CalculatorWindow.text = "" + _currentNumber;
-
+            SetCurrentNumber(NumberOnButton, _number1);
         }
         else
         {
-            _number2 = _number2 != 0 ? (_number2 * 10) + NumberOnButton : NumberOnButton;
-            _currentNumber = _number2;
-            CalculatorWindow.text = "" + _currentNumber;
+            SetCurrentNumber(NumberOnButton, _number2);
         }
     }
+    private void SetCurrentNumber(int NumberOnButton, float InputNumber)
+    {
+        InputNumber = InputNumber != 0 ? (InputNumber * 10) + NumberOnButton : NumberOnButton;
+        _currentNumber = InputNumber;
+        CalculatorWindow.text = "" + _currentNumber;
+    }
+
     private float GetFloatedNumber(int NumberOnButton, float CurrentNumber)
     {
-        string shouldBeFloat = CurrentNumber.ToString() + "." + NumberOnButton.ToString();
+        string shouldBeFloat = (CurrentNumber).ToString() + "." + NumberOnButton.ToString();
         return float.Parse(shouldBeFloat, CultureInfo.InvariantCulture.NumberFormat);
     }
     public void DotPressed()
     {
         dotIsPressed = true;
-        CalculatorWindow.text += ".";
-        Debug.Log("dotIsPressed = " + dotIsPressed + "Currentnumber = " + _currentNumber);
+        CalculatorWindow.text += ",";
     }
     public void OperationPressed(string op)
     {
@@ -67,16 +85,15 @@ public class calc : MonoBehaviour
         //save 1st number
         _number1 = _currentNumber;
         operation = op;
-        _number2 = 0.0f;
+         _number2 = 0.0f;
         CalculatorWindow.text = _number1.ToString() + " " + operation + " ";
-
-        Debug.Log("operationPressed = " + operationIsPressed + "operation = " + operation);
     }
     public void EqualPressed()
     {
         if (string.IsNullOrEmpty(operation))
         {
             CalculatorWindow.text = _number1.ToString();
+            _currentNumber = _number1;
         }
         else
         {
@@ -84,8 +101,8 @@ public class calc : MonoBehaviour
         }
         CalculatorWindow.text = _result.ToString();
         _currentNumber = _result;
-        _result = 0.0f;
     }
+
     private void Calculate()
     {
         switch (operation)
@@ -109,16 +126,26 @@ public class calc : MonoBehaviour
             case "*":
                 _result = _number1 * _currentNumber;
                 break;
+            case "%":
+                _result = _number1 % _currentNumber;
+                break;
             default:
                 break;
         }
     }
 
-/*    private bool IsFloatedNumber(float number)
-    {
-        string str = number.ToString();
-        return str.Contains(".");
-    }*/
+    /*  хз как это работает, но не работает
+        private float Calculate()
+        {
+            return operation switch
+            {
+                "+" => _number1 + _currentNumber,
+                "-" => _number1 - _currentNumber,
+                "*" => _number1 * _currentNumber,
+                "/" => _number1 / _currentNumber,
+                _ => throw new ArgumentException("Недопустимый код операции")
+            };
+        }*/
 
     public void ButtonC()
     {
